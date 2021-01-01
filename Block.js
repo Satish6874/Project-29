41d9b4eae99e535090d7f454292986cfa42d4ddd
Satish6874 <74088874+Satish6874@users.noreply.github.com>
2021-01-01 10:54:16 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,18 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      fill(9,174,196);
+      rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);
+      
+    }
+  }
